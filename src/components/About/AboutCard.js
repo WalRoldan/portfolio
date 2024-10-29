53765d4534bf11161fae16bcb0bac5e8d887a164
WalRoldan <wallrottweila@gmail.com>
2024-10-29 10:49:04 -0300

    fix

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Walter Roldan</span>
+            Hi Everyone, I am <span className="purple">Walter Roldan </span>
             from <span className="purple"> Cordoba, Argentina.</span>
             <br />
             I am currently employed as a software developer.
